a7e5ebbc420bbce0719ea67b474e22afac52b073
inoxtg <theophile.bourget.sio@gmail.com>
2023-03-17 15:58:52 +0100

    todos + reflexion sur turf ?

--- GSB-RV-Visiteur/app/src/main/java/fr/gsb/rv/visiteur/ConsulterActivity.kt
+++ GSB-RV-Visiteur/app/src/main/java/fr/gsb/rv/visiteur/ConsulterActivity.kt
@@ -6,7 +6,22 @@ import android.view.View
 import android.widget.TextView
 import fr.gsb.rv.visiteur.dialogs.DeconnectionDialog
 import fr.gsb.rv.visiteur.dialogs.RetourDialog
-
+/*
+TODO :
+ -Definir des textview avec :
+     Numero de rapport
+     Date visite
+     Date redac
+     Pra nom
+     Pra prenom
+     Pra cp
+     Pra ville
+  -Instancier avec les valeurs du plus récent rapport (premier dans la list vu que ASC)
+  -OnClick 'valider' modifier ces valeurs avec celles retournées
+  -Carrousel avec les rv du mois et année selectionnées
+  ---------Pas de caroussel mais des petites icones des rapport puis onClick affichage  ?  |||||||||||||||| Me semble la meilleure idée cf carnet pour visu
+  ---------Si un seul caroussel invisible ?
+ */
 class ConsulterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
